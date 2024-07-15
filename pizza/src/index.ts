71319c485f4518546e62196fda471d2c6c1b539e
kseniiaIrinarkhova <kseniiairinarkhova@gmail.com>
2024-07-15 12:16:03 -0700

    Pizza store schema

--- pizza/src/index.ts
+++ pizza/src/index.ts
@@ -22,13 +22,32 @@ set name(newName:string){
 }
 
 class Cheese extends Ingredient{
-    constructor(price:number){
-        super(price,'Cheese');
+    constructor(price?:number){
+        super((price)?price:2.5,'Cheese');
     }
 }
 class Pepperoni extends Ingredient{
-    constructor(price:number){
-        super(price,'Pepperoni');
+    constructor(price?:number){
+        super((price)?price:2.5,'Pepperoni');
+    }
+}
+class Daugh extends Ingredient{
+    constructor(price?:number){
+        super((price)?price:8,'Daugh');
+    } 
+}
+
+class ThinCrispDaugh extends Daugh{
+    constructor(){
+        super();
+        this.name = 'Thin Crisp Daugh'
+    }
+}
+
+class ThickCheesyDaugh extends Daugh{
+    constructor(){
+        super();
+        this.name = "Thick Cheesy Daugh";
     }
 }
 
@@ -42,22 +61,26 @@ class DoubbleIngredient extends Ingredient{
     get price():number{
         return this.#ingredient.price *2;
     }
+    get name():string{
+        return `Double ${this.#ingredient.name}`;
+    }
+}
+class DoubleCheese extends DoubbleIngredient{
+    constructor(){
+        super(new Cheese());
+    }
 }
 
 abstract class Pizza{
     #ingredients:Array<Ingredient>;
-    #basePrice:number;
     #name:string;
-    constructor(name?:string, basePrice?:number, ingredients?:Array<Ingredient>){
+    constructor(name?:string, ingredients?:Array<Ingredient>){
         this.#ingredients = (ingredients)?[...ingredients]:[];
         this.#name = (name)?name:"New Pizza";
-        this.#basePrice = (basePrice)?basePrice:0;
     }
-    set basePrice(newBasePrice:number){
-        this.#basePrice = newBasePrice;
-    }
+    
     getPrice():number{
-        let pizzaPrice = this.#basePrice;
+        let pizzaPrice = 0;
         this.#ingredients.forEach((ingredient)=>{
             pizzaPrice += ingredient.price;
         })
@@ -66,4 +89,79 @@ abstract class Pizza{
     get name():string{
         return this.#name;
     }
+    addIngredient(ingredient: Ingredient){
+        this.#ingredients.push(ingredient);
+    }
+    prepare():void{
+    console.log(`Start to prepare pizza: ${this.#name}`);
+    console.log(`add ingredients:`);
+    this.#ingredients.forEach((ingredient)=>{
+        console.log(`Add ${ingredient.name}`);
+    })
+    console.log(`Pizza is ready for you!`)
+    }
 }
+
+class CheesePizza extends Pizza{
+    constructor(daugh:Daugh){
+        super("Margarita");
+        this.addIngredient(daugh);
+        this.addIngredient(new DoubleCheese());
+    }
+}
+
+class PepperoniPizza extends Pizza{
+    constructor(daugh:Daugh){
+        super("Pepperoni");
+        this.addIngredient(daugh);
+        this.addIngredient(new Cheese());
+    }
+}
+
+class PizzaFactory{
+    public createPizza(name:string, isThin:boolean=true):Pizza{
+        switch(name){
+            case 'Margarita':
+                return this.createCheesePizza(isThin);
+                break;
+            case 'Pepperoni':
+                return this.createPepperoni(isThin);
+                break;
+            default:
+                return this.createCheesePizza(isThin);
+                break;
+        }
+    }
+    public createCheesePizza(isThin:boolean=true):CheesePizza{
+        return (isThin)?new CheesePizza(new ThinCrispDaugh()): new CheesePizza(new ThickCheesyDaugh());
+    }
+    public createPepperoni(isThin:boolean=true):PepperoniPizza{
+        return (isThin)?new PepperoniPizza(new ThinCrispDaugh()):new PepperoniPizza(new ThickCheesyDaugh());
+    }
+}
+
+
+class PizzaStore{
+    constructor(){
+        console.log('Welcome to our store!')
+    }
+    getOrder(pizzaName:string, daugh?:string){
+        let pizza: Pizza;
+        let isThin:boolean = daugh === 'thin';
+        let pizzaFactory = new PizzaFactory();
+        pizza = pizzaFactory.createPizza(pizzaName,isThin);
+        this.prepareOrder(pizza);
+    }
+    prepareOrder(pizza:Pizza){
+        console.log(`*******New Order**************`)
+        pizza.prepare();
+        console.log(`Pizza's price: ${pizza.getPrice()}`);
+    }
+}
+
+let pizzaStore = new PizzaStore();
+
+pizzaStore.getOrder('Margarita','thin');
+pizzaStore.getOrder('Margarita','thick');
+pizzaStore.getOrder('Pepperoni','thin');
+pizzaStore.getOrder('Pepperoni','thick');
